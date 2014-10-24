5cfa746355715b20386e6d9522831abc37db36e7
Dmitriy Kuhar <dmitriykuhar@tut.by>
2014-10-25 01:45:56 +0300

    fixes

--- src/main/java/com/test/project/MainClass.java
+++ src/main/java/com/test/project/MainClass.java
@@ -1,7 +1,17 @@
 package com.test.project;
 
-/**
- * Created by Family on 24.10.2014.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
 public class MainClass {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello(ModelMap model) {
+        model.addAttribute("message", "hello world");
+        return "hello";
+    }
 }
