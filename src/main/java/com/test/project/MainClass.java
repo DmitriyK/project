cf6fcb7fb8801a8a2916e2e4bf84b6d59eea42e9
Dmitriy Kuhar <dmitriykuhar@tut.by>
2014-10-26 23:01:43 +0300

    fixes

--- src/main/java/com/test/project/MainClass.java
+++ src/main/java/com/test/project/MainClass.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class MainClass {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String hello(ModelMap model) {
+    @RequestMapping(value = "/he", method = RequestMethod.GET)
+    public String abc(ModelMap model) {
         model.addAttribute("message", "hello world");
         return "hello";
     }
