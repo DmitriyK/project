2cd6c1d3ab5719b505fb97469ad889d31ff32773
dmitriy.kuhar <dmitriykuhar@tut.by>
2014-12-18 13:47:20 +0300

    added logger

--- src/main/java/com/test/project/TestController.java
+++ src/main/java/com/test/project/TestController.java
@@ -1,5 +1,6 @@
 package com.test.project;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -11,19 +12,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class TestController {
 
+    private static final Logger logger = Logger.getLogger(TestController.class);
+
     @Autowired
     ApplicationContext applicationContext;
 
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
         model.addAttribute("message", "Hello Mops!");
+        logger.info("welcome request");
         return "hello";
     }
 
     @RequestMapping(value = "/he", method = RequestMethod.GET)
     public String abc(ModelMap model) {
-        if(applicationContext !=null)
         model.addAttribute("message", "I love my mops");
+        logger.error("error");
         return "hello";
     }
 }
