4a4cb94296eea90ba146fe553fd72e3eb736d1df
Dmitriy Kuhar <dmitriykuhar@tut.by>
2014-12-20 02:32:55 +0300

    added methods to interface CommonDao

--- src/main/java/com/test/project/dao/common/CommonDaoImpl.java
+++ src/main/java/com/test/project/dao/common/CommonDaoImpl.java
@@ -2,6 +2,32 @@ package com.test.project.dao.common;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class CommonDaoImpl implements CommonDao{
+    @Override
+    public void addEntity(Object entity) {
+
+    }
+
+    @Override
+    public void updateEntity(Object entity) {
+
+    }
+
+    @Override
+    public void deleteEntity(Object entity) {
+
+    }
+
+    @Override
+    public Object getEntity(int id) {
+        return null;
+    }
+
+    @Override
+    public List toList() {
+        return null;
+    }
 }
