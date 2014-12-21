76b73b28ec7a34e312e559f4e21ba66cb49e6290
Dmitriy Kuhar <dmitriykuhar@tut.by>
2014-12-22 00:05:32 +0300

    added CommonDao

--- src/main/java/com/test/project/dao/common/CommonDao.java
+++ src/main/java/com/test/project/dao/common/CommonDao.java
@@ -1,4 +1,20 @@
 package com.test.project.dao.common;
 
+import org.hibernate.Session;
+
+import java.util.List;
+
 public interface CommonDao {
+
+    public Session getSession();
+
+    public <T> void addEntity(T entity);
+
+    public <T> void updateEntity(T entity);
+
+    public <T> void deleteEntity(T entity);
+
+    public <T> T getEntity(int id, Class<T> clazz);
+
+    public <T> List<T> getList(Class<T> clazz);
 }
