11c671469acd37e8c54b70500cd9104c6521bb7a
dmitriy.kuhar <dmitriykuhar@tut.by>
2014-12-22 19:14:19 +0300

    fix errors

--- src/main/java/com/test/project/dao/impl/UserDaoImpl.java
+++ src/main/java/com/test/project/dao/impl/UserDaoImpl.java
@@ -4,13 +4,17 @@ import com.test.project.dao.common.CommonDaoImpl;
 import com.test.project.dao.iface.UserDao;
 import com.test.project.domain.User;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class UserDaoImpl extends CommonDaoImpl implements UserDao {
 
     @Override
+    @Transactional
     public User findByUserName(String username) {
-        User user = (User) getSession().createQuery("from user where username=?").setParameter(0, username).uniqueResult();
+        User user = (User) getSession().createQuery("from User where username = :username").setParameter("username", username).uniqueResult();
+        System.out.println("");
         return user;
     }
 }
